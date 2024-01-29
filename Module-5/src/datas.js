6b05544a240c3599d9b0d307c01171e3f5193786
Dheeraj Mathur <122856357+dheerajmat@users.noreply.github.com>
2024-01-29 23:38:25 +0530

    Create datas.js

---
+++ Module-5/src/datas.js
@@ -0,0 +1,20 @@
+const shoeData = [
+    {
+        "imageLink": "https://images.unsplash.com/photo-1588099768523-f4e6a5679d88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHwxMTM4MTU1NXx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+        "name": "Nike Airmax v2"
+    },
+    {
+        "imageLink": "https://images.unsplash.com/photo-1588099768523-f4e6a5679d88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHwxMTM4MTU1NXx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+        "name": "Nike Airmax v2"
+    },
+    {
+        "imageLink": "https://images.unsplash.com/photo-1588099768523-f4e6a5679d88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHwxMTM4MTU1NXx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+        "name": "Nike Airmax v2"
+    },
+    {
+        "imageLink": "https://images.unsplash.com/photo-1588099768523-f4e6a5679d88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHwxMTM4MTU1NXx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+        "name": "Nike Airmax v2"
+    }
+];
+
+export default shoeData;
